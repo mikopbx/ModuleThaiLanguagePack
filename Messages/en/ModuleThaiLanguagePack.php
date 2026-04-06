aa50b7f88132f58bdc1e67c989a6c41cb5ed5960
Nikolay Beketov <nbek@miko.ru>
2026-04-06 14:21:48 +0700

    Initial commit: ModuleThaiLanguagePack language pack for MikoPBX

---
+++ Messages/en/ModuleThaiLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleThaiLanguagePack' => 'Thai Language Pack',
+    'SubHeaderModuleThaiLanguagePack' => 'Complete Thai language support for MikoPBX',
+];
---
+++ Messages/ru/ModuleThaiLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleThaiLanguagePack' => 'Языковой пакет - Тайский',
+    'SubHeaderModuleThaiLanguagePack' => 'Комплексная поддержка тайского языка для системы MikoPBX',
+];
